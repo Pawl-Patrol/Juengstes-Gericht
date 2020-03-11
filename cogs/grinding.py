a32e84664bb3f6aa6b8a07dbc8924fae424c2b3d
Pawl123 <57140776+Pawl123@users.noreply.github.com>
2020-03-11 17:44:21 +0100

    Update grinding.py

--- cogs/grinding.py
+++ cogs/grinding.py
@@ -14,71 +14,90 @@ class Grinding(commands.Cog, command_attrs=dict(cooldown_after_parsing=True)):
     def __init__(self, bot):
         self.bot = bot
         self.con = connection
-        with open("data/crafting.json", "r") as f:
+        with open("data/crafting.json", "r", encoding="UTF-8") as f:
             self.recipes = json.load(f)
 
     @commands.command(usage="craft <item>", aliases=["crafting"])
     @commands_only()
-    async def craft(self, ctx, page: int = 1):
+    async def craft(self, ctx, *, item: str = None):
         """Carfting-Commands"""
         inv = self.con["inventory"].find_one({"_id": ctx.author.id})
-        items = list(self.con["items"].find())
-        emojis = {item["_id"]: item["emoji"] for item in items}
-        pages = len(self.recipes)
-        if page < 1:
-            page = 1
-        elif page > pages:
-            page = pages
+        items = self.con["items"].find()
+        tools = list(self.con["tools"].find())
+        emojis = {item["_id"]: item["emoji"] for item in list(items) + tools}
+        tools = [t["_id"] for t in tools]
+        if not item:
+            page = 0
+            pages, b = divmod(len(self.recipes), 5)
+            if b != 0:
+                pages += 1
 
-        def create_embed(page):
-            item = list(self.recipes.keys())[page-1]
-            ingredients = self.recipes[item]
+            def create_embed(p):
+                embed = discord.Embed(
+                    color=discord.Color.blue(),
+                    title=f"Crafting ({p+1}/{pages})",
+                    description=f"Benutze `{ctx.prefix}craft <item>`, um ein Item zu craften.\n"
+                )
+                for crafting_item, ingredients in [(a, b) for a, b in self.recipes.items()][p * 5:p * 5 + 5]:
+                    lines, remainer = divmod(len(ingredients), 5)
+                    if remainer != 0:
+                        lines += 1
+                    recipe = ""
+                    for i in range(lines):
+                        ing = []
+                        for ingredient, count in list(ingredients.items())[i * 5:i * 5 + 5]:
+                            if ingredient in tools:
+                                continue
+                            ing.append(f"{count}x {emojis[ingredient]}")
+                        recipe += "\n> " + " ".join(ing)
+                    embed.description += f"\n**» {emojis[crafting_item]} __{crafting_item.title()}__**:{recipe}"
+                return embed
+
+            menu = await ctx.send(embed=create_embed(page))
+            if pages > 1:
+                reactions = ['◀', '▶']
+                for reaction in reactions:
+                    await menu.add_reaction(reaction)
+                while True:
+                    try:
+                        reaction, user = await ctx.bot.wait_for("reaction_add", check=lambda r, u: r.message.id == menu.id and u == ctx.author and str(r.emoji) in reactions, timeout=60)
+                    except asyncio.TimeoutError:
+                        await menu.clear_reactions()
+                        return
+                    await menu.remove_reaction(reaction, user)
+                    if str(reaction.emoji) == reactions[0] and page > 0:
+                        page -= 1
+                    elif str(reaction.emoji) == reactions[1] and page < pages-1:
+                        page += 1
+                    await menu.edit(embed=create_embed(page))
+        elif item.lower() in self.recipes:
+            recipe = self.recipes[item.lower()]
             embed = discord.Embed(
                 color=discord.Color.green(),
-                title=f"{emojis[item]} {item.title()} ({page}/{len(self.recipes)})",
-                description=f"❥ ~ `{ctx.prefix}craft {item}`"
+                title=item.title(),
+                description=""
             )
             missing = False
-            for ingredient, count in ingredients.items():
+            for ingredient, count in recipe.items():
                 amount = inv.get(ingredient, 0)
-                embed.description += f"\n> **{amount}/{count}** {ingredient.title()} {emojis[ingredient]}"
-                if amount < count:
+                if amount >= count:
+                    amount = count
+                else:
                     missing = True
+                embed.description += f"\n> **{amount}/{count}** {ingredient} {emojis[ingredient]}"
             if missing:
                 embed.color = discord.Color.red()
-            return embed, missing
-
-        embed, missing = create_embed(page)
-        menu = await ctx.send(embed=embed)
-        if pages > 1:
-            reactions = ['◀', '☑️', '▶']
-            for reaction in reactions:
-                await menu.add_reaction(reaction)
-
-            def check(r, u):
-                return r.message.id == menu.id and u == ctx.author and str(r.emoji) in reactions
-
-            while True:
+                await ctx.send(embed=embed)
+            else:
+                msg = await ctx.send(embed=embed)
+                await msg.add_reaction("☑️")
                 try:
-                    reaction, user = await ctx.bot.wait_for("reaction_add", check=check, timeout=60)
+                    reaction, user = await ctx.bot.wait_for("reaction_add", check=lambda r, u: r.message.id == msg.id and u == ctx.author and str(r.emoji) == "☑️", timeout=60)
                 except asyncio.TimeoutError:
-                    await menu.clear_reactions()
+                    await msg.clear_reactions()
                     return
-                await menu.remove_reaction(reaction, user)
-                if str(reaction.emoji) == reactions[0] and page > 1:
-                    page -= 1
-                elif str(reaction.emoji) == reactions[1] and not missing:
-                    await menu.delete()
-                    item = list(self.recipes.keys())[page-1]
-                    post = {k: -v for k, v in self.recipes[item].items()}
-                    post[item] = 1
-                    self.con["inventory"].update({"_id": ctx.author.id}, {"$inc": post}, upsert=True)
-                    await ctx.send(f"{ctx.author.mention} Du hast **1x {item.title()} {emojis[item.lower()]}** gecraftet!")
-                    return
-                elif str(reaction.emoji) == reactions[2] and page < pages:
-                    page += 1
-                embed, missing = create_embed(page)
-                await menu.edit(embed=embed)
+        else:
+            await ctx.send(f"{ctx.author.mention} Ich konnte dieses Item nicht finden.")
 
     @commands.command()
     @commands.cooldown(1, 300, commands.BucketType.user)
