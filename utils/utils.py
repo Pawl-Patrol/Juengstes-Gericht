f43d9c8fa98a086d29123eaa0aa253af900cbb7f
Pawl123 <57140776+Pawl123@users.noreply.github.com>
2020-03-12 15:31:17 +0100

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 def convert_upgrade_levels(mult_lvl, money_lvl, crit_lvl):
     """Errechnet den Wert eines Upgrade-Levels"""
     mult = 100 + mult_lvl * 5
-    money = money_lvl + money_lvl * 2 + 2
+    money = round(money_lvl * 1.8) + 2
     crit = round(crit_lvl * 1.5)
     return mult, money, crit
 
